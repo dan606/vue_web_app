87a30294a4ad01b64ac016ce5c020f41f01a962b
Daniel Hron <daniel.hron@ales.aero>
2022-04-04 22:16:02 +0200

    Pridano odeslani postpro login

--- src/components/HelloI18n.vue
+++ src/components/HelloI18n.vue
@@ -4,12 +4,87 @@
    <widget-container-modal/>
 
   <button type="button" class="btn btn-primary" @click="showModal=true">Open a modal</button>
-  <Modal v-model="showModal" :title="t('settings')">
-  <VueModalWindow/>
-</Modal>
+  <!--
+  <VueModal v-model="showModal" :title="t('settings')">
+    <VueModalWindow/>
+  </VueModal>
+  -->
 
+<div v-if="showModal" class="modal-mask">
+ <div class="modal-wrapper">
+   <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="btn-close" @click="showModal = false" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <!-- <p>Modal body text goes here.</p> -->
+        <VueModalWindow/>
+      </div>
+
+      <!--
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" @click="showModal = false">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+      -->
+
+    </div>
+    </div>
+  </div>
+</div>
+
+ <!-- <div v-if="showModal">
+    <transition name="modal">
+      <div class="modal-mask">
+        <div class="modal-wrapper">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true" @click="showModal = false">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <p>Modal body text goes here.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" @click="showModal = false">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
+  -->
 
 </template>
+
+
+<style scoped>
+
+.modal-mask {
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .5);
+  display: table;
+  transition: opacity .3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+</style>
 
 <script>
 import { defineComponent } from 'vue'
@@ -17,10 +92,11 @@ import { useI18n } from 'vue-i18n'
 import axios from 'axios'
 import {openModal, container} from "jenesius-vue-modal";
 import ModalWindow from "./ModalWindow.vue"
-import VueModalWindow from "./VueModalWindow.vue"
+import VueModalWindow from '@/components/VueModalWindow.vue'
 
 import VueModal from '@kouts/vue-modal'
 import '@kouts/vue-modal/dist/vue-modal.css'
+
 
   export default {
   name: 'HelloI18n',
@@ -35,8 +111,8 @@ import '@kouts/vue-modal/dist/vue-modal.css'
     return { t }
   },
   components: {WidgetContainerModal: container,
-                    'Modal': VueModal,
-                    VueModalWindow: VueModalWindow},
+                    VueModal,
+                    VueModalWindow},
   methods:
   {
     openModalWindow()
@@ -48,7 +124,8 @@ import '@kouts/vue-modal/dist/vue-modal.css'
   },  
   data() {
     return {
-      showModal: false
+      showModal: false,
+      mes: ''
     }
   }
 }
--- src/components/VueModalWindow.vue
+++ src/components/VueModalWindow.vue
@@ -1,11 +1,14 @@
 <template>
-    <div>
         <p>{{t('settings')}}</p>
-    </div>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="email" v-model="mail">
+        <input type="password" class="form-control" id="exampleFormControlInput2" placeholder="password" v-model="password">
+        <button type="button" class="btn btn-primary" @click="login">Login</button>
 </template>
 
 <script>
 import { useI18n } from 'vue-i18n'
+import axios from 'axios'
+
 export default {
 	name: "VueModal",
     setup() {
@@ -17,10 +20,27 @@ export default {
         // Something todo ..
 
         return { t }
+    },
+    data() 
+    {
+        return {
+            mail: '',
+            password: ''
+        }
+    },
+    methods: {
+        login() {
+        console.log("send login")
+        //this.infoColor = 'blue';
+        axios({
+            method: 'post',
+            url: 'http://localhost:3000/login',
+            data: {
+            name: this.mail,
+            password: this.password
+            }
+        });
+      }
     }
 }
 </script>
-
-<style scoped>
-
-</style>
